f6bc053bc0c12a0b5c03d50dc19fbcaa51c4d679
ArianFiroozi <arianfiroozi3@gmail.com>
2025-05-01 11:39:51 +0330

    feat(hardware): add sensor listener

--- UTAirestrike/app/src/main/java/com/example/utairestrike/src/hardware/SensorConnector.java
+++ UTAirestrike/app/src/main/java/com/example/utairestrike/src/hardware/SensorConnector.java
@@ -1,4 +1,49 @@
 package com.example.utairestrike.src.hardware;
 
-public class SensorConnector {
+import static android.hardware.Sensor.TYPE_GRAVITY;
+import static android.hardware.Sensor.TYPE_GYROSCOPE;
+import static android.hardware.Sensor.TYPE_MAGNETIC_FIELD;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+public class SensorConnector extends Activity implements SensorEventListener {
+    private final SensorManager mSensorManager;
+    private final Sensor mAccelerometer;
+    private final Sensor mGyroscope;
+    private final Sensor mGravimeter;
+    private final Sensor mMagentometer;
+
+    public SensorConnector() {
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mGyroscope = mSensorManager.getDefaultSensor(TYPE_GYROSCOPE);
+        mGravimeter = mSensorManager.getDefaultSensor(TYPE_GRAVITY);
+        mMagentometer = mSensorManager.getDefaultSensor(TYPE_MAGNETIC_FIELD);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mGyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mGravimeter, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mMagentometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        var sensorName = sensorEvent.sensor.getName();
+        System.out.println(sensorName + ": X: " + sensorEvent.values[0] +
+                "; Y: " + sensorEvent.values[1] +
+                "; Z: " + sensorEvent.values[2] + ";");
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int acc) {}
 }
